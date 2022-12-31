10bbf225cf2bd4fdec485e89be580d4114ec22f6
coderpak <1919701172@qq.com>
2022-12-31 21:26:22 +0800

    修改index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 // import App from './02_购物车案例/App';
 // import "./02_购物车案例/App.css"
@@ -12,7 +12,7 @@ import ReactDOM from 'react-dom/client';
 // import App from './09_作用域插槽实现/App'
 // import App from './10_非父子组件通信-Context/App'
 // import App from './11_非父子组件通信-EventBus/App'
-import App from './12_setState详细使用/App'
+import App from "./12_setState详细使用/App";
 // import App from './13_render函数的优化/02_pureComponent/App'
 // import App from './14_数据不可变的力量/App'
 // import App from './15_ref获取dom元素/App'
@@ -20,6 +20,7 @@ import App from './12_setState详细使用/App'
 // import App from './17_高阶组件/App'
 // import App from './18_Portals的使用/App'
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App rootData={{ name: "pak", age: 20 }} />);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App rootData={{name:'pak',age:20}} />);
+console.log("-----");
